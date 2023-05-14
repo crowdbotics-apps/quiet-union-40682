f260e69da8c5df71fe42fe8d874465876bc31c42
Crowdbotics <team@crowdbotics.com>
2023-05-14 14:22:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-union-40682",
+  url: "https://quiet-union-40682.botics.co",
+  api: "https://quiet-union-40682.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
